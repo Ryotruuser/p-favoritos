ccfb63f24f3f55aaebc3f3a134b04e6b83503483
Ryotruuser <ryonantruuser@gmail.com>
2025-01-26 15:55:50 -0300

    adicionado elementos favoritos, corrigido footer, ajustado alturas das imagens favoritas

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function mudaCorTextoAnime(){
+    const texto = document.getElementsByClassName("favorito_titulo");
+    texto[0].style.color = "#fc8feb";
+}
+
+function retornarPadraoCor(){
+    const texto = document.getElementsByClassName("favorito_titulo");
+    texto[0].style.color = "#c2f0fe";      
+}
