7e901f14de97fedaa4d9d4e2da142298ab06f59c
Ryotruuser <ryonantruuser@gmail.com>
2025-01-26 16:09:55 -0300

    ajustado hover dos favoritos

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,48 @@
-function mudaCorTextoAnime(){
+function element0(){
     const texto = document.getElementsByClassName("favorito_titulo");
     texto[0].style.color = "#fc8feb";
 }
-
+function element1(){
+    const texto = document.getElementsByClassName("favorito_titulo");
+    texto[1].style.color = "#fc8feb";
+}
+function element2(){
+    const texto = document.getElementsByClassName("favorito_titulo");
+    texto[2].style.color = "#fc8feb";
+}
+function element3(){
+    const texto = document.getElementsByClassName("favorito_titulo");
+    texto[3].style.color = "#fc8feb";
+}
+function element4(){
+    const texto = document.getElementsByClassName("favorito_titulo");
+    texto[4].style.color = "#fc8feb";
+}
+function element5(){
+    const texto = document.getElementsByClassName("favorito_titulo");
+    texto[5].style.color = "#fc8feb";
+}
+function element6(){
+    const texto = document.getElementsByClassName("favorito_titulo");
+    texto[6].style.color = "#fc8feb";
+}
+function element7(){
+    const texto = document.getElementsByClassName("favorito_titulo");
+    texto[7].style.color = "#fc8feb";
+}
+function element8(){
+    const texto = document.getElementsByClassName("favorito_titulo");
+    texto[8].style.color = "#fc8feb";
+}
+function element9(){
+    const texto = document.getElementsByClassName("favorito_titulo");
+    texto[9].style.color = "#fc8feb";
+}
 function retornarPadraoCor(){
     const texto = document.getElementsByClassName("favorito_titulo");
-    texto[0].style.color = "#c2f0fe";      
+    for (let index = 0; index < 9; index++) {
+        texto[index].style.color = "#c2f0fe"; 
+        
+    }
+         
 }
